e719c309bf8d793a1fd630490f65f2f7b3f0f5cd
quietimcoding <dmonte3210@gmail.com>
2024-05-06 10:21:43 -0700

    small change to make file more readable

--- process-file.sh
+++ process-file.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 METADATA_FILE="${1:-$(</dev/stdin)}"
 
-JQ_FILTER='.output.abi[] | select ((.anonymous | not) and .type == "event") | .name+"("+(.inputs | map(.type) | join(","))+")",.name+"("+(.inputs | map(.type+(if .indexed then " indexed" else "" end)+" "+.name) | join(","))+")",.name+"("+(.inputs | map(.type+(if .indexed then " indexed" else "" end)) | join(","))+")"'
+JQ_FILTER='.output.abi[] | select ((.anonymous | not) and .type == "event") | 
+    .name+"("+(.inputs | map(.type) | join(","))+")",.name+"("+(.inputs | map(.type+(if .indexed then " indexed" else "" end)+" "+.name) | 
+    join(","))+")",.name+"("+(.inputs | map(.type+(if .indexed then " indexed" else "" end)) | 
+    join(","))+")"'
 cat $METADATA_FILE | jq "$JQ_FILTER" -r | tr \\n \\0 | xargs -0 -L3 ./process-sig.sh
